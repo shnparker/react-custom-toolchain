4ae7c9b349a277db987e246d401e5679d46c6194
Shane Parker <sparker2@godaddy.com>
2023-02-15 16:49:53 +0200

    demonstrate importing asset through webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import image from '@/assets/cover_banner.png'
 import test from '@/utils/test'
 
 function App() {
@@ -8,6 +9,12 @@ function App() {
       <div className="text-2xl text-blue-500">Custom React Toolchain</div>
       <div className="text-2xl text-red-500">{test()}</div>
       <div className="text-2xl text-red-500">{process.env.TEST_VARIABLE ?? 'No variable found'}</div>
+
+      <p>Import from public folder</p>
+      <img src={process.env.PUBLIC_URL + '/images/cover_banner.png'} alt="" />
+
+      <p>Import from src folder</p>
+      <img src={image} alt="" />
     </>
   )
 }
