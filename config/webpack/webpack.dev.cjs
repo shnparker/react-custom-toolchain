c7a4f2e3f0c9acaf0f499bd9167b427b26fec808
Shane Parker <sparker2@godaddy.com>
2023-03-15 15:11:51 +0200

    small changes to project

--- config/webpack/webpack.dev.cjs
+++ config/webpack/webpack.dev.cjs
@@ -1,7 +1,7 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
 
-const common = require('./webpack.common.js')
+const common = require('./webpack.common')
 
 const configPaths = require('../configPaths')
 
